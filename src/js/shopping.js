f1761c4bd5b6f820faac98899fe28ebbd2f8466d
Michael P <michael.paterson7@gmail.com>
2022-07-27 21:37:59 -0700

    created product-data.json

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -1,10 +1,33 @@
-// Cart item class
+// Cart item data
+import productData from "./data/product-data.json";
+console.log(productData);
+/* 
+{
+    name: get item name from product card
+    price: get price from target product's price dataset
+}
+
+*/
 
 // When 'add to cart' button is pressed
-    // construct cart item
+    // create new cart object
     // create html template of item, fill title and price with associated data
 
     // add item price to subtotal
+
+    // change "Add to Cart" text to "Added to Cart"
+    // disable add to cart button button
+
+// Adding an item to cart
+    // get data from target product card {title, price}
+    // post data to shopping cart
+    // add item price to subotal
+
+// Removing an item from the cart
+    // subtract item price from subtotal
+    // change card button from "Added to Cart" to "Add to Cart"
+    // enable add to cart button
+    // destroy cart item data
 
 // Calculating and displaying subtotal, tax, total
     // every new cart item adds its price to the subtotal
@@ -13,3 +36,11 @@
     // tax = subtotal * 0.725
     // total = subtotal + tax
     
+
+// Set up Local Storage
+
+    // add to storage
+
+    // update storage
+
+    // remove from storage
